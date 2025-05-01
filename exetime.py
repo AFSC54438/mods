8b003cb39c55b8787d1dbfa3f6c187452a8de49f
AFSC54438 <123247460+AFSC54438@users.noreply.github.com>
2025-05-01 16:21:47 +0700

    updated exetime.py
    made it so that it can compute execution time for recursive functions

--- exetime.py
+++ exetime.py
@@ -1,10 +1,21 @@
 import time
+from functools import wraps
 
 def exe_time(f):
+    in_progress = False
+
+    @wraps(f)
     def wrapper(*args, **kwargs):
+        nonlocal in_progress
+        if in_progress:
+            return f(*args, **kwargs)
+        
+        in_progress = True
         start = time.perf_counter()
         result = f(*args, **kwargs)
         end = time.perf_counter()
+        in_progress = False
+
         print(f"Execution time ({f.__name__}): {(end - start)*1000:.5f} ms")
         return result
     return wrapper
